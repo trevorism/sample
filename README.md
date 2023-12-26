d3adcf21ed34467e52a4d82e1eaa630ac1b8ad3c
Trevor Brooks <trevorism@gmail.com>
2023-12-26 15:24:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# sample
+![Build](https://github.com/trevorism/sample/actions/workflows/deploy.yml/badge.svg)
+![GitHub last commit](https://img.shields.io/github/last-commit/trevorism/sample)
+![GitHub language count](https://img.shields.io/github/languages/count/trevorism/sample)
+![GitHub top language](https://img.shields.io/github/languages/top/trevorism/sample)
+
+# How to build
+`gradle clean build`
---
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -0,0 +1,22 @@
+package com.trevorism
+
+import groovy.transform.CompileStatic
+import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "App",
+                version = "0.0.1",
+                description = "API",
+                contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
+        )
+)
+@CompileStatic
+class Application {
+    static void main(String[] args) {
+        Micronaut.run(Application, args)
+    }
+}
